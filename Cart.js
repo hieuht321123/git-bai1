6c5cb3bac373035d1068eefec0e8453bbccf7f43
hieuht <hieuht321123>
2021-10-13 15:03:31 +0700

    git revert change

--- Cart.js
+++ Cart.js
@@ -1,11 +1,4 @@
 function addCat() {
     console.log("hello");
-}
 
-function loadCart() {
-    console.log("chao nguoi ban moi den");
-}
-
-function loadCart2() {
-    console.log("chao nguoi ban moi den");
 }
